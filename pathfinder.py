69b85d17393fdaf2c618677a77667e488fe55154
pedromicael <pedromicael@gmail.com>
2019-09-07 19:57:34 -0300

    getPathsFromBurpFile

--- pathfinder.py
+++ pathfinder.py
@@ -2,12 +2,14 @@ from urllib.parse import unquote, urlparse
 from pathlib import PurePosixPath
 import argparse
 import sys
-
-
 from xml.etree import ElementTree
 import pprint
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-url", help="urlFormat: http://example.com/l1/l2/l3/l4")
+parser.add_argument("-b", help="burp xml file")
+parser.add_argument("-f", help="read urlFormat: http://example.com/l1/l2/l3/l4 from file")
 
 
 
@@ -21,7 +23,6 @@ def getPaths(url):
             ).path
         )
     ).parts
-    print(url)
     #print(url[len(url)-1])
     if(url[len(url)-1].find("/"))==-1:
         temp = temp + ('f',)
@@ -32,7 +33,6 @@ def getPaths(url):
 
 
 def splitPaths(paths):
-    print(paths)
     level = 0
     len_paths = len(paths)
     
@@ -40,38 +40,77 @@ def splitPaths(paths):
         for path in paths:
             level = level +1
             if (level == len_paths-1):
-                print(paths[len_paths-1].find("f"))
                 if(paths[len_paths-1].find("f"))!=-1:
-                    writeWordLevelFile(path,0)
+                    print(checkDuplicatePath(0,path))
+                    if(not checkDuplicatePath(0,path)):
+                        #print(path)
+                        writeWordLevelFile(path,0)
                 else:
-                    writeWordLevelFile(path,level)
+                    if(not checkDuplicatePath(level,path)):
+                        #print(path)
+                        writeWordLevelFile(path,level)
             elif (level != len_paths):
-                writeWordLevelFile(path,level)
+                if(not checkDuplicatePath(level,path)):
+                    #print(path)
+
+                    writeWordLevelFile(path,level)
            
+
+def checkDuplicatePath(level,path_check):
+    try:
+        with open("L"+str(level)+".txt","r") as file:
+            for path in file:
+                if path.find(path_check) >=0:
+                    return True
+                    #print(path)
+            
+            file.close()
+    except FileNotFoundError:
+        return False
+    return False
 
 
 
 def writeWordLevelFile(path,level):
-    file = open(str(level)+".txt","a+")
+    file = open("L"+str(level)+".txt","a+")
     file.write(path+"\n")
+    file.close
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-url", help="urlFormat: http://example.com/l1/l2/l3/l4")
+
+def getPathsFromBurpFile():
+    with open(args.burp, 'rt') as f:
+        tree = ElementTree.parse(f)
+
+    for node in tree.iter():
+        if(node.find("url") is not None):
+            paths = getPaths(node.find("url").text)
+            splitPaths(paths)
+        #print(node.text)
+
+
+
+def getPathsFromTxtFile(filePath):
+    try:
+        with open(filePath,"r") as file:
+            for url in file:
+                paths = getPaths(url)
+                splitPaths(paths)
+            file.close()
+    except FileNotFoundError:
+        return False
+    return False
+
 args = parser.parse_args()
 
+if args.url:
+    paths = getPaths(args.url)
+    splitPaths(paths)
+elif args.b:
+    getPathsFromBurpFile()
+elif args.f:
+    getPathsFromTxtFile(args.f)
 
-paths = getPaths(args.url)
-splitPaths(paths)
 
-
-with open('path_to_your_xml.xml', 'rt') as f:
-    tree = ElementTree.parse(f)
-
-for node in tree.iter():
-    if(node.find("url") is not None):
-        paths = getPaths(node.find("url").text)
-        splitPaths(paths)
-    #print(node.text)
 
 
 
