f657670995fc57635ccc0e3193b422046bf87fd2
pedromicael <pedromicael@gmail.com>
2019-09-07 15:02:32 -0300

    writeWordLevelFile

--- pathfinder.py
+++ pathfinder.py
@@ -1,9 +1,18 @@
 from urllib.parse import unquote, urlparse
 from pathlib import PurePosixPath
 import argparse
+import sys
+
+
+from xml.etree import ElementTree
+import pprint
+
+
+
+
 
 #get paths in Posix Path format
-#return as a tuple removing the last (/) character
+#return as a tuple removing the first (/) character
 def getPaths(url):
     temp = PurePosixPath(
         unquote(
@@ -12,15 +21,34 @@ def getPaths(url):
             ).path
         )
     ).parts
+    print(url)
+    #print(url[len(url)-1])
+    if(url[len(url)-1].find("/"))==-1:
+        temp = temp + ('f',)
+    else:
+        temp = temp + ('d',)
+
     return (temp[ : 0 ] + temp[1 : ])
 
 
 def splitPaths(paths):
+    print(paths)
     level = 0
-    if paths:
+    len_paths = len(paths)
+    
+    if len_paths>1:
         for path in paths:
             level = level +1
-            writeWordLevelFile(path,level)
+            if (level == len_paths-1):
+                print(paths[len_paths-1].find("f"))
+                if(paths[len_paths-1].find("f"))!=-1:
+                    writeWordLevelFile(path,0)
+                else:
+                    writeWordLevelFile(path,level)
+            elif (level != len_paths):
+                writeWordLevelFile(path,level)
+           
+
 
 
 def writeWordLevelFile(path,level):
@@ -34,6 +62,19 @@ args = parser.parse_args()
 
 paths = getPaths(args.url)
 splitPaths(paths)
+
+
+with open('path_to_your_xml.xml', 'rt') as f:
+    tree = ElementTree.parse(f)
+
+for node in tree.iter():
+    if(node.find("url") is not None):
+        paths = getPaths(node.find("url").text)
+        splitPaths(paths)
+    #print(node.text)
+
+
+
 
 
 
