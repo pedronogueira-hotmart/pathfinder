768fa7e863093e18b930581ae1fc0d92a2e12ec1
pedromicael <pedromicael@gmail.com>
2019-10-16 11:57:41 -0300

    Clear option.

--- pathfinder.py
+++ pathfinder.py
@@ -4,14 +4,58 @@ import argparse
 import sys
 from xml.etree import ElementTree
 import pprint
+import glob
+
+import os
+import glob
+import re
+
+
+
+globalCount=0
+numberOfLines=0
+
+
+def getDomain(url):
+    domain = re.search("^.[^/?&]+", url)
+    print(domain.group(0))
+    return domain.group(0)
+
+
+
+def listFiles(path):
+    files = [f for f in glob.glob(path + "**/L*.txt", recursive=True)]
+    return files
+
+
+
+def deleteFiles(files):
+    for file in files:
+        print(file)
+    if files:
+        answer= input('These files will be removed. Are you sure? (y/n): ')
+        if (answer=='y' or answer=='Y'):
+            for file in files:
+                os.remove(file)
+        elif (answer=='n' or answer=='n'):
+            sys.exit()
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-url", help="urlFormat: http://example.com/l1/l2/l3/l4")
 parser.add_argument("-b", help="burp xml file")
 parser.add_argument("-f", help="read urlFormat: http://example.com/l1/l2/l3/l4 from file")
+parser.add_argument("-l", help="choice the path level", type=int)
+parser.add_argument("-c", help="consolidate L* files in a single out file")
+parser.add_argument("-d", help="delete L* files", action='store_true')
+parser.add_argument("-t", help="TLD")
 
-globalCount=0
+#check if any arguments have been passed 
+# if(len(sys.argv)==1):
+#     parser.print_help()
+#     sys.exit()
+
+
 
 
 def getNumberOfLines(filePath):
@@ -32,9 +76,9 @@ def getNumberOfLines(filePath):
 #return as a tuple removing the first (/) character
 def getPaths(url):
     global globalCount
-    global numberOfLines
+
     globalCount = globalCount +1
-    print(url,(globalCount/numberOfLines)*100)
+    print(url,(globalCount/1)*100)
 
     temp = PurePosixPath(
         unquote(
@@ -92,7 +136,7 @@ def checkDuplicatePath(level,path_check):
 
 
 def writeWordLevelFile(path,level):
-    file = open("L"+str(level)+".txt","a+")
+    file = open(getDomain(path)+"_"+"L"+str(level)+".txt","a+")
     file.write(path+"\n")
     file.close
 
@@ -107,6 +151,33 @@ def getPathsFromBurpFile():
             splitPaths(paths)
         #print(node.text)
 
+def consolodateFiles(path_L_files):
+    print(path_L_files+"L*.txt")
+    print(sorted(glob.glob(path_L_files+"L*.txt")))
+    L_files = sorted(glob.glob(path_L_files+"L*.txt"))
+
+    size_L_files = len(L_files)
+
+    for i in range(size_L_files):
+        #descartar size = 1 se L0.txt existir
+
+        with open(l_file,'r') as paths:
+            for path in paths:
+                print(path)
+
+    
+
+
+        print(L_files[i])
+
+
+    for l_file in sorted(glob.glob(path_L_files+"L*.txt")):
+        with open(l_file,'r') as paths:
+            for path in paths:
+                print(path)
+    
+
+    return 1
 
 
 def getPathsFromTxtFile(filePath):
@@ -127,12 +198,15 @@ if args.url:
     splitPaths(paths)
 elif args.b:
     getPathsFromBurpFile()
+elif args.c:
+    consolodateFiles(args.c)
 elif args.f:
-    global numberOfLines
     numberOfLines = getNumberOfLines(args.f)
     getPathsFromTxtFile(args.f)
-
-
+elif args.d:
+    deleteFiles(listFiles(os.getcwd()))
+elif args.t:
+    getDomain(args.t)
 
 
 
